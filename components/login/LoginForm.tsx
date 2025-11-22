4dc830a427c120c33a1eed5af5a2665747ea3dc5
poom-ytthpch <yutthapichai.cha@ext.axonstech.com>
2025-11-23 00:25:32 +0700

    feat(ui): add component login form

--- components/login/LoginForm.tsx
+++ components/login/LoginForm.tsx
@@ -1,13 +1,28 @@
 "use client";
+import { useAppDispatch } from "@/store/hooks";
+import { login } from "@/store/slice/auth.slice";
+import { LoginInput, LoginResponse } from "@/types/gql";
 import { Button, Form, Input } from "antd";
+import { useRouter } from "next/navigation";
 
 const LoginForm = () => {
+  const dispatch = useAppDispatch();
+  const router = useRouter();
   const [form] = Form.useForm();
+
+  const handleLogin = async (values: LoginInput) => {
+    const res = await dispatch(login(values));
+    const payload: any = (res as any)?.payload;
+    const data: LoginResponse = payload?.data?.login;
+    if (data.status) {
+      router.push("/");
+    }
+  };
 
   return (
     <div className="h-[350px] md:w-[500px] md:h-[400px] rounded-3xl p-6 shadow-lg bg-white/80  backdrop-blur-sm md:grid grid-cols-1 grid-rows-2 md:grid-cols-2  md:grid-rows-1">
       <div className=" md:flex md:justify-center md:items-center">
-        <Form form={form} layout="vertical">
+        <Form form={form} layout="vertical" onFinish={handleLogin}>
           <Form.Item name="email" label="Email" className="">
             <Input type="text" />
           </Form.Item>
