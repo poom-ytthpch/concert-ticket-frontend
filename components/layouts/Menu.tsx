de09b4bf022aabc2ba015a08bc9f3277fe0d6bdd
poom-ytthpch <yutthapichai.cha@ext.axonstech.com>
2025-11-23 01:06:44 +0700

    feat(ui):add menu list

---
+++ components/layouts/Menu.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { Menu } from "antd";
+import { HomeOutlined, InboxOutlined, SyncOutlined } from "@ant-design/icons";
+import { useRouter } from "next/navigation";
+import { useAppSelector } from "@/store/hooks";
+const MenuList = () => {
+  const router = useRouter();
+  const auth = useAppSelector((state) => state.auth);
+
+  const menuItems = [
+    {
+      key: "home",
+      icon: <HomeOutlined />,
+      label: "Home",
+    },
+    {
+      key: "history",
+      icon: <InboxOutlined />,
+      label: "History",
+    },
+    {
+      key: "switch_to_user",
+      icon: <SyncOutlined />,
+      label: "Switch to User",
+    },
+    {
+      key: "switch_to_admin",
+      icon: <SyncOutlined />,
+      label: "Switch to Admin",
+    },
+  ];
+
+  const handleClick = (e: { key: string }) => {
+    if (e.key === "home") {
+      router.push(`/`);
+      return;
+    }
+    router.push(`/${e.key}`);
+  };
+
+  return (
+    <Menu
+      mode="inline"
+      onClick={handleClick}
+      items={menuItems.map((item) => {
+        if (item.key === "switch_to_user" && auth.user?.isAdmin) {
+          return null;
+        } else if (item.key === "switch_to_admin" && !auth.user?.isAdmin) {
+          return null;
+        } else {
+          return item;
+        }
+      })}
+    />
+  );
+};
+
+export default MenuList;
